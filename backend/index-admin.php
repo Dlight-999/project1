a20913c8d15946c04c7ef2a2e184537654042a7c
Dlight-999 <dracokd19@gmail.com>
2023-07-22 07:19:12 +0545

    Changes to dashboard

--- backend/index-admin.php
+++ backend/index-admin.php
@@ -1,4 +1,5 @@
-<?php include"nav.php" ?>
+<?php include "/config/constants.php" ?>
+<?php include "nav.php" ?>
 
       <!-- Main -->
       <main class="main-container">
@@ -15,7 +16,15 @@
               <p class="text-primary">Users</p>
               <span class="material-icons-outlined text-blue">inventory_2</span>
             </div>
-            <span class="text-primary font-weight-bold">249</span>
+            <span class="text-primary font-weight-bold">
+            <?php
+              $sql_user = "SELECT * FROM users";
+              $res = mysqli_query($conn, $sql_user);
+              if($res == TRUE){
+                echo $count = mysqli_num_rows($res);
+              }            
+            ?>
+            </span>
           </div>
 
           <div class="card">
@@ -23,7 +32,15 @@
               <p class="text-primary">Packages</p>
               <span class="material-icons-outlined text-orange">add_shopping_cart</span>
             </div>
-            <span class="text-primary font-weight-bold">83</span>
+            <span class="text-primary font-weight-bold">
+            <?php
+              $sql_package = "SELECT * FROM activities";
+              $res = mysqli_query($conn, $sql_package);
+              if($res == TRUE){
+                echo $count = mysqli_num_rows($res);
+              }            
+            ?>
+            </span>
           </div>
 
           <div class="card">
@@ -31,7 +48,15 @@
               <p class="text-primary">Booking</p>
               <span class="material-icons-outlined text-green">shopping_cart</span>
             </div>
-            <span class="text-primary font-weight-bold">79</span>
+            <span class="text-primary font-weight-bold">
+            <?php
+              $sql_booking = "SELECT * FROM booking";
+              $res = mysqli_query($conn, $sql_booking);
+              if($res == TRUE){
+                echo $count = mysqli_num_rows($res);
+              }            
+            ?>
+            </span>
           </div>
 
           
